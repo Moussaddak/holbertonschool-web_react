ac207801cad15404b34a73a1e4766613a494c7d0
Moussaddak <meddeb.moussaddak@gmail.com>
2021-10-31 13:29:06 +0100

    up

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -2,19 +2,18 @@ import $ from 'jquery';
 const _ = require('lodash');
 
 $.when( $.ready ).then(function() {
-    let count = 0;
+
+    let counter = 0;
     function updateCounter() {
-        count++;
-        $('#count').html(`${count} clicks on the button`);
+        counter++;
+        $('#count').html(`${counter} clicks on the button`);
     };
 
-    $('body').append(
-        $('<p>').text('Holberton Dashboard'),
-        $('<p>').text('Dashboard data for the students'),
-        $('<button>').text('Click here to get started'),
-        $('<p>').attr('id', 'count'),
-        $('<p>').text('Copyright - Holberton School'),
-    )
+    $('body').append('<p>Holberton Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    $('body').append('<button>Click here to get started</button>');
+    $('body').append($('<p>').attr('id', 'count'));
+    $('body').append('<p>Copyright - Holberton School</p>');
 
     $('button').on('click', _.debounce(updateCounter, 500));
 });
