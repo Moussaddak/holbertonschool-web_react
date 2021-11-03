bc67cb099e99c2ab7a74387bc09e0238e9e2e5ad
Moussaddak <meddeb.moussaddak@gmail.com>
2021-11-03 17:43:17 +0100

    up

---
+++ 0x02-react_intro/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import './Notifications.css';
+
+// const Notification = () => <div className="Notifications">
+//     <p>Here is the list of notifications</p>
+// </div>
+class Notification extends Component {
+    render() {
+        return (
+            <div className="Notifications">
+                <p>Here is the list of notifications</p>
+            </div>
+        );
+    }
+}
+export default Notification;
